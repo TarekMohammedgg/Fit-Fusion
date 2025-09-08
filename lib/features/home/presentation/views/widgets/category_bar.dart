f6ffc4ecc4af0e7b9f51146170c7a121783821dd
= <tarekmohammed022@gmail.com>
2025-09-08 05:11:15 +0300

    create category section

---
+++ lib/features/home/presentation/views/widgets/category_bar.dart
@@ -0,0 +1,75 @@
+import 'package:fit_fusion/features/home/data/models/catergory_model.dart';
+import 'package:fit_fusion/features/home/presentation/views/widgets/category_item.dart';
+import 'package:fit_fusion/features/home/presentation/views/widgets/exercise_model.dart';
+import 'package:flutter/material.dart';
+
+class CategoryBar extends StatefulWidget {
+  final Function(Set<String>) onFilterChanged;
+
+  const CategoryBar({super.key, required this.onFilterChanged});
+
+  @override
+  State<CategoryBar> createState() => _CategoryBarState();
+}
+
+class _CategoryBarState extends State<CategoryBar> {
+  final List<CatergoryModel> categories = [
+    CatergoryModel(title: ExercisFilter.All.name),
+    CatergoryModel(title: ExercisFilter.Push.name),
+    CatergoryModel(title: ExercisFilter.Pull.name),
+    CatergoryModel(title: ExercisFilter.Legs.name),
+  ];
+
+  Set<String> selectedFilters = {ExercisFilter.All.name};
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 5,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          final title = categories[index].title;
+          final isSelected = selectedFilters.contains(title); // true , false 
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: CategoryItem(
+              title: title,
+              isSelected: isSelected,
+              onSelected: (value) {
+                setState(() {
+                  if (title == ExercisFilter.All.name) {
+                    if (value) {
+                      selectedFilters = {ExercisFilter.All.name};
+                    } else {
+                      if (selectedFilters.length == 1 &&
+                          selectedFilters.contains(ExercisFilter.All.name)) {
+                        return; 
+                      } else {
+                        selectedFilters.remove(title);
+                      }
+                    }
+                  } else {
+                    if (value) {
+                      selectedFilters.remove(ExercisFilter.All.name);
+                      selectedFilters.add(title);
+                    } else {
+                      selectedFilters.remove(title);
+                      if (selectedFilters.isEmpty) {
+                        selectedFilters = {ExercisFilter.All.name};
+                      }
+                    }
+                  }
+
+                  widget.onFilterChanged(selectedFilters);
+                });
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
